2c9aa898476f59ba252f1174698a85fa11797175
Raza <rtshareef@gmail.com>
2019-11-14 18:52:57 -0500

    redux_codaAlong

--- js/reducer.js
+++ js/reducer.js
@@ -1,13 +1,24 @@
+let state = {count: 0};
+ 
 function changeState(state, action){
-  switch (action.type) {
-    case 'INCREASE_COUNT':
-      return {count: state.count + 1}
-    default:
-      return state;
+    switch (action.type) {
+      case 'INCREASE_COUNT':
+        return {count: state.count + 1}
+      default:
+        return state;
+    }
   }
+ 
+function render(){
+    document.body.textContent = state.count
 }
+ 
+function dispatch(action){
+  state = changeState(state, action)
+  render()
+}
+ 
 
-let state = {count: 0}
-let action = {type: 'INCREASE_COUNT'}
-
-changeState(state, action)
+// dispatch({type: 'INCREASE_COUNT'})
+// dispatch({type: 'INCREASE_COUNT'})
+render()
